b886b9f71fe667a1ebf7183ed5a436726f33ef27
Iaroslav <eremeev.pt@gmail.com>
2023-01-07 15:24:35 +0200

    Update custom exceptions

--- src/main/java/program/Program.java
+++ src/main/java/program/Program.java
@@ -1,32 +1,9 @@
 package program;
 
-import com.sun.security.jgss.GSSUtil;
-import model.Entry;
 import util.Util;
-
-import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) {
-        /*// Head of the hierarchy
-        Entry granddad = new Entry("Granddad");
-        // Head's children, constructor by parent
-        Entry dad = new Entry(granddad);
-        dad.setName("Dad");
-        Entry uncle = new Entry(granddad);
-        uncle.setName("Uncle");
-        // Dad's children, constructor by parent and name separately
-        Entry me = new Entry(dad);
-        me.setName("Me");
-        Entry brother = new Entry("Brother");
-        dad.addChild(brother);
-        // Uncle's child, constructor by name and parent together
-        Entry cousin = new Entry("Cousin", uncle);
-        // Show all the elements beginning from the head
-        granddad.showChildren();*/
-
-        // Testing treeBuilder
-        Scanner scanner = new Scanner(System.in);
-        Util.treeBuilder(scanner);
+        Util.treeBuilder();
     }
 }
--- src/main/java/util/Util.java
+++ src/main/java/util/Util.java
@@ -2,42 +2,51 @@ package util;
 
 import model.Entry;
 
+import java.util.Arrays;
 import java.util.InputMismatchException;
 import java.util.Objects;
 import java.util.Scanner;
 
 public class Util {
-
-    public static void treeBuilder(Scanner scanner){
-        System.out.println("Please start building the tree");
-        System.out.println("First type the root object name");
-        System.out.println("Then continue filling the tree by typing parent name followed by child name");
-        System.out.println("To quit type two equivalent names");
+    public static void treeBuilder(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Start building the tree");
+        System.out.println("First type the root object name. It should be written as only one word");
         String firstInput = scanner.nextLine();
         String[] firstElements = firstInput.split(" ");
         if (firstElements.length == 1){
+            System.out.println("Great! The root object name is " + firstElements[0]);
+            System.out.println("Now continue filling the tree by typing parent element name followed by a child element name");
+            System.out.println("Name of a child element must be unique!");
+            System.out.println("To quit type two equivalent words");
             Entry rootEntry = new Entry(firstElements[0]);
-            String input = scanner.nextLine();
-            String[] inputElements = input.split(" ");
-            while (!Objects.equals(inputElements[0], inputElements[1])){
-                if (inputElements.length != 2) {
-                    throw new InputMismatchException("You should type only 2 names - " +
-                            "first is for parent and second is for child");
+            String input;
+            String[] inputElements = new String[0];
+            do {
+                try {
+                    input = scanner.nextLine();
+                    inputElements = input.split(" ");
+                    if (Objects.equals(inputElements[0], inputElements[1])) break;
+                    if (inputElements.length != 2 || rootEntry.checkUniqueName(inputElements[1])) {
+                        throw new InputMismatchException();
+                    }
+                    System.out.println("Great! Parent element is " + inputElements[0] +
+                            " and child element is " + inputElements[1]);
+                    Entry parent = rootEntry.findEntry(inputElements[0]);
+                    parent.addChild(new Entry(inputElements[1]));
+                } catch (InputMismatchException e) {
+                    System.out.println("You should type only 2 element names: " +
+                            "first for the parent and second for a child\n" +
+                            "Name of a child element must be unique!");
                 }
-                if (rootEntry.checkUniqueName(inputElements[1])) {
-                    throw new InputMismatchException("Name of the child must be unique!");
-                }
-                Entry parent = rootEntry.findEntry(inputElements[0]);
-                parent.addChild(new Entry(inputElements[1]));
-                input = scanner.nextLine();
-                inputElements = input.split(" ");
-            }
-            System.out.println("Now let's see the whole tree!");
+            } while (!Objects.equals(inputElements[0], inputElements[1]));
+            System.out.println("Now let's see the whole tree!\n");
             rootEntry.showChildren();
         }
         else {
-            System.out.println("You should start by creating the root object. Type only one name");
+            System.out.println("You should start by creating the root object. It should be written as only one word");
         }
-
     }
+
+
 }
